b54539423d32a245b896dfffa8d626a62959432b
Low Tier God <domaic95@kingshigh.school.nz>
2022-09-07 12:26:22 +1200

    deadly group start

--- Scenes/SwingingTrap.gd
+++ Scenes/SwingingTrap.gd
@@ -4,7 +4,3 @@ func _ready():
 	$AnimationPlayer.play("Swinging")
 	pass
 
-func _on_DeathZone_area_entered(area):
-	if area.in_in_group("Deadly"):
-		if GameStats.check_reset() == false:
-			global_position = GameStats.get_spawn().global_position
