b688ab32a7619a7a1b19976369b37d4eb5994ab4
Low Tier God <domaic95@kingshigh.school.nz>
2022-10-17 12:27:17 +1300

    file

---
+++ Scenes/DoornButton.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$AnimationPlayer.play("ButtonUp")
+	$AnimationPlayer.play("DoorClosed")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Area2D_body_entered(body):
+	if body.is_in_group("Player"):
+		$AnimationPlayer.play("ButtonDown")
+		$AnimationPlayer.play("DoorOpen")
